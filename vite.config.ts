220f11dfa16f118d6951b05f3b8e45349a9af4eb
zpecter <sychrat@gmail.com>
2025-02-09 08:28:01 +0100

    build chunk size fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          axios: ['axios'],
+          dayjs: ['dayjs'],
+          emotion: ['@emotion/react', '@emotion/styled'],
+          mui: ['@mui/icons-material', '@mui/material', '@mui/x-date-pickers'],
+          react: ['react', 'react-dom', 'react-router-dom'],
+          zod: ['zod'],
+          zustand: ['zustand'],
+        },
+      },
+    },
+  },
 });
